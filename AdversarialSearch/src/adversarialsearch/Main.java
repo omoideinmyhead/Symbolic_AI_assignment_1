5a408a3f3b8cf7772f928c4f21429b789513933c
ianbdehaan <ianbertholdhaan@gmail.com>
2023-10-15 22:44:10 +0200

    mostly added comments and did some minor changes

--- AdversarialSearch/src/adversarialsearch/Main.java
+++ AdversarialSearch/src/adversarialsearch/Main.java
@@ -4,16 +4,9 @@ import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) {
-		State table = new State();
-		try {
-			table.read("data/board.txt");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		//System.out.println(table.minimax(table,0,13,0));
-		Game g=new Game();
+
+		Game g = new Game();
 		g.test();
-			
+
 	}
 }
--- AdversarialSearch/src/adversarialsearch/State.java
+++ AdversarialSearch/src/adversarialsearch/State.java
@@ -23,20 +23,25 @@ public class State {
 		this.food = 0;
 		this.moves = new Vector<String>();
 	}
-
+	
+	//constructs a State object via a file containing a board
 	public void read(String file) throws IOException {
+		//reads first line to figure out the number of rows and columns
 		Scanner readfile = new Scanner(new File(file));
 		String firstLine = readfile.nextLine();
 		String[] segments = firstLine.split(" ");
 		int columns = Integer.parseInt(segments[0]);
 		int rows = Integer.parseInt(segments[1]);
+		//initiate the board
 		this.board = new char[rows][columns];
 		int j = 0;
+		//iterate through all lines of the .txt file filling the entries of board and adding additional info to the state
 		while (j < rows) {
 			int i = 0;
 			String line = readfile.nextLine();
 			while (i < columns) {
 				char currentChar = line.charAt(i);
+				//when the chars are A or B we update AgentX and AgentY and fill the board with a space character
 				if (currentChar == 'A') {
 					agentX[0] = i;
 					agentY[0] = j;
@@ -45,10 +50,12 @@ public class State {
 					agentX[1] = i;
 					agentY[1] = j;
 					board[j][i] = ' ';
+				//in the other cases we just fill the board with the respective char in the .txt file
 				} else {
 					board[j][i] = currentChar;
 				}
 				;
+				//we also use this opportunity to count the amount of food
 				if (currentChar == '*') {
 					food += 1;
 				}
@@ -59,7 +66,9 @@ public class State {
 		}
 	}
 
+	//method that displays the state
 	public String toString() {
+		//we first iterate through the board chars adding them to a printable string
 		String printable = "board:\n";
 		for (char[] C : board) {
 			for (char c : C) {
@@ -67,6 +76,7 @@ public class State {
 			}
 			printable += "\n";
 		}
+		//the we use string format method to display the other relevant info
 		printable += String.format("agentX: (%d,%d)\n", agentX[0], agentX[1]);
 		printable += String.format("agentY: (%d,%d)\n", agentY[0], agentY[1]);
 		printable += String.format("score: (%d,%d)\n", score[0], score[1]);
@@ -75,12 +85,15 @@ public class State {
 		return printable;
 	}
 
+	//method designed to copy a State object by value
 	public State copy() {
 		State state2 = new State();
+		//as board is an array of arrays we need to clone line by line
 		state2.board = new char[board.length][];
 		for (int i = 0; i < board.length; i++) {
 			state2.board[i] = board[i].clone();
 		}
+		//for other arrays .clone() is enough
 		state2.agentX = agentX.clone();
 		state2.agentY = agentY.clone();
 		state2.food = food;
@@ -88,30 +101,38 @@ public class State {
 		state2.turn = turn;
 		return state2;
 	}
-
+	
+	//method designed to return the legal moves
 	public Vector<String> legalMoves(int agent) {
+		//first saves the location of the current agent
 		int[] Loc = { agentY[agent], agentX[agent] };
 		Vector<String> moves = new Vector<String>();
+		//when there is food at agent location we add eat to actions
 		if (board[Loc[0]][Loc[1]] == '*') {
 			moves.add("eat");
 		}
 		;
+		//when there's food or empty space to the right we add right to actions
 		if (board[Loc[0]][Loc[1] + 1] == ' ' || board[Loc[0]][Loc[1] + 1] == '*') {
 			moves.add("right");
 		}
 		;
+		//when there's food or empty space downwards we add down to actions
 		if (board[Loc[0] + 1][Loc[1]] == ' ' || board[Loc[0] + 1][Loc[1]] == '*') {
 			moves.add("down");
 		}
 		;
+		//when there's food or empty space upwards we add down to actions
 		if (board[Loc[0] - 1][Loc[1]] == ' ' || board[Loc[0] - 1][Loc[1]] == '*') {
 			moves.add("up");
 		}
 		;
+		//when there's food or empty space to the left we add left to actions
 		if (board[Loc[0]][Loc[1] - 1] == ' ' || board[Loc[0]][Loc[1] - 1] == '*') {
 			moves.add("left");
 		}
 		;
+		//when there's an empty space at agent location we add block to actions
 		if (board[Loc[0]][Loc[1]] == ' ') {
 			moves.add("block");
 		}
@@ -121,52 +142,57 @@ public class State {
 
 	}
 
+	//helper function that just return the legal moves for the agent currently with turn
 	public Vector<String> legalMoves() {
 		return legalMoves(turn);
 	}
 
+	//execute method responsible for executing an action
 	public void execute(String action) {
+		//simple switch block to handle all possible actions
 		switch (action) {
-		case "eat":
-			score[turn]++;
-			board[agentY[turn]][agentX[turn]] = ' ';
-			food--;
-			break;
-		case "up":
-			agentY[turn]--;
-			break;
-		case "down":
-			agentY[turn]++;
-			break;
-		case "left":
-			agentX[turn]--;
-			break;
-		case "right":
-			agentX[turn]++;
-			break;
-		case "block":
-			board[agentY[turn]][agentX[turn]] = '#';
-			break;
+			case "eat":
+				score[turn]++;
+				board[agentY[turn]][agentX[turn]] = ' ';
+				food--;
+				break;
+			case "up":
+				agentY[turn]--;
+				break;
+			case "down":
+				agentY[turn]++;
+				break;
+			case "left":
+				agentX[turn]--;
+				break;
+			case "right":
+				agentX[turn]++;
+				break;
+			case "block":
+				board[agentY[turn]][agentX[turn]] = '#';
+				break;
 		}
-		if (turn == 1) {
-			turn = 0;
-		} else {
-			turn = 1;
-		}
-		;
+		//changes the agent with turn
+		turn = ((turn == 1) ? 0 : 1);
 	}
 
+	//method responsible for evaluating if a state is a leaf
 	public boolean isLeaf() {
+		//when there are no legal moves for the player with turn or when there is no more food the state is a leaf
 		if (legalMoves().isEmpty()) {
 			return true;
 		} else if (food == 0) {
 			return true;
 		}
+		//the state is not a leaf otherwise
 		return false;
 	}
 
+	//method responsible for evaluating the local value of a state for a given agent
 	public double value(int agent) {
+		//first saves who the other agent is
 		int otherAgent = ((agent == 0) ? 1 : 0);
+		//in the case where the food is over if someone has a higher score they win
 		if (food == 0) {
 			if (score[agent] > score[otherAgent]) {
 				return 1;
@@ -174,12 +200,14 @@ public class State {
 				return -1;
 			}
 		}
+		//if someone has the turn and there are no legal moves they lose
 		if (turn == otherAgent && legalMoves(otherAgent).isEmpty()) {
 			return 1;
 		}
 		if (turn == agent && legalMoves(agent).isEmpty()) {
 			return -1;
 		}
+		//all other states have a local value of 0
 		return 0;
 	}
 }
