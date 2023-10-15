ed32e0c2809610134f79b45920a0d0e6bfdcb0d1
ianbdehaan <ianbertholdhaan@gmail.com>
2023-10-15 14:31:23 +0200

    now with counter for states accessed

--- AdversarialSearch/src/adversarialsearch/Game.java
+++ AdversarialSearch/src/adversarialsearch/Game.java
@@ -5,6 +5,8 @@ import java.util.Vector;
 
 public class Game {
 	State b;
+	int countMinimax;
+	int countAlfabeta;
 
 	public Game() {
 		b = new State();
@@ -15,10 +17,11 @@ public class Game {
 			e.printStackTrace();
 		}
 	}
-
+	
 	public State minimax(State s, int forAgent, int maxDepth, int depth) {
 		// base case, leaf or maxDepth just returns itself
 		if (s.isLeaf() || depth == maxDepth) {
+			countMinimax++;
 			return s;
 		}
 		// general case finds the best value possible (max or min) and returns the state
@@ -29,6 +32,7 @@ public class Game {
 		double min = 2;
 		// loop goes through all legal moves and finds the best move considering if the player is forAgent or not
 		for (String move : s.legalMoves()) {
+			countMinimax++;
 			// copies the state to execute the move, necessary because we have to remember the original state for the next moves
 			State nextState = s.copy();
 			nextState.execute(move);
@@ -53,9 +57,10 @@ public class Game {
 		return returnState;
 	}
 
-	public State alphabeta(State s, int forAgent, int maxDepth, int depth, double alpha, double beta) {
+	public State alfabeta(State s, int forAgent, int maxDepth, int depth, double alfa, double beta) {
 		// base case, leaf or maxDepth just returns itself
 		if (s.isLeaf() || depth == maxDepth) {
+			countAlfabeta ++;
 			return s;
 		}
 		// general case finds the best value possible (max or min) and returns the state
@@ -66,11 +71,12 @@ public class Game {
 		double min = 2;
 		// loop goes through all legal moves and finds the best move considering if the player is forAgent or not
 		for (String move : s.legalMoves()) {
+			countAlfabeta ++;
 			// copies the state to execute the move, necessary because we have to remember the original state for the next moves
 			State nextState = s.copy();
 			nextState.execute(move);
 			// we recursively find the result state and save its value in resultValue to optimize the rest of the code (don't have to evaluate it 3 times)
-			State searchResult = alphabeta(nextState, forAgent, maxDepth, depth + 1, alpha, beta);
+			State searchResult = alfabeta(nextState, forAgent, maxDepth, depth + 1, alfa, beta);
 			double resultValue = searchResult.value(forAgent);
 			// if the turn belongs to the forAgent we want the state of max value
 			if (forAgent == s.turn) {
@@ -81,11 +87,11 @@ public class Game {
 				if (resultValue > max) {
 					returnState = searchResult;
 					max = resultValue;
-					alpha = max;
+					alfa = max;
 				}
 			} else {
-				// if the value is lower than or equal alpha this subtree will not be relevant in actual play and we can already return the result
-				if (resultValue <= alpha) {
+				// if the value is lower than or equal alfa this subtree will not be relevant in actual play and we can already return the result
+				if (resultValue <= alfa) {
 					return searchResult;
 				}
 				if (resultValue < min) {
@@ -100,8 +106,14 @@ public class Game {
 	}
 
 	public void test() {
-
-		System.out.println(alphabeta(b, b.turn, 11, 0, -2, 2));
+		for (int i=7;i<14;i+=2 ) {
+			countMinimax = 0;
+			minimax(b, b.turn, i, 0);
+			countAlfabeta = 0;
+			alfabeta(b, b.turn, i, 0, -2, 2);
+			System.out.println(String.format("turn %d: minimax = %d runs; alfabeta = %d runs", i, countMinimax, countAlfabeta));
+		}
+		System.out.println(alfabeta(b, b.turn, 11, 0, -2, 2));
 
 //		while (!b.isLeaf()){
 //			System.out.println(b.toString());
